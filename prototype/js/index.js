9fda5c8b993a7e8dc1f0ce27049cd9cbccfafadd
berlina <berlina.lili@gmail.com>
2017-02-17 12:50:56 +0800

    add prototype
    add prototype

---
+++ prototype/js/index.js
@@ -0,0 +1,19 @@
+// maker gif  videotaobao/
+//add intro 
+// test speed - whether buy a new domain. 
+//update linkedin 
+
+
+
+// function link_work(){
+//   $('.project').fadeIn(); 
+//   $('body').animate({scrollTop:$('.project').offset().top},1000);
+// };  
+
+// $(document).ready(function(){
+//   $('.linktowork').click(function(){
+//      link_work();
+//  });
+
+  
+// });

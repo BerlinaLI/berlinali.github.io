8a950dd2d53733c07786c19fab2df8929ff62cc5
berlina <berlina.lili@gmail.com>
2017-03-01 18:34:39 +0800

    add disco invite

---
+++ disco invite/sketch.js
@@ -0,0 +1,25 @@
+var gif;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  gif = loadGif('dance.gif');
+}
+
+function draw() {
+  background(255);
+  var imageheight = gif.height/gif.width * window.innerWidth;
+  image(gif, 0, 0,window.innerWidth,gif.height/gif.width * window.innerWidth);
+  
+  noStroke();
+	if(frameCount % 7 < 3 ){
+	  noFill(); //pic 33%  12 
+	} else if(frameCount % 7 > random(3,4) ){
+		fill(255);//white 33% 56 
+	} else{
+		fill(0);//black 34% 
+	}
+	rect(0,0,window.innerWidth,window.innerHeight);
+}
+
+
+

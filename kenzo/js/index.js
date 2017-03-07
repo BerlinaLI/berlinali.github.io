45842fb3615f7980c2a451d71d30d1f5a3202683
berlina <berlina.lili@gmail.com>
2017-03-07 16:28:06 +0800

    add twitter share feature

--- kenzo/js/index.js
+++ kenzo/js/index.js
@@ -20,11 +20,21 @@ function randomRef(){
   document.getElementById("author").innerHTML = reference[index].author; 
   document.getElementById("pic").src=reference[index].pic;
 }              
-              
-function myFunction() { 
+
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+
+function shareTwitter(){
+  openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));
+}
+
+function getQuote() { 
   randomRef(); 
 }
 
+getQuote();
 
 
 // var quotes =["dd","duuuuuuuuuuuuud","qeeeeeeeeeeeeee"];

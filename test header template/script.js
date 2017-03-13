57040886baad5ac46af454b2b1e53acd5e2fff61
berlina <berlina.lili@gmail.com>
2017-03-13 17:38:47 +0800

    add settimeout

--- test header template/script.js
+++ test header template/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
  $('#nav-container').load('https://berlinali.github.io/header #nav');
- $('a.navwork').addClass("active");
+ 
+  setTimeout(function(){ 
+  	$('a.navwork').addClass("active");
+  }, 500);
+ 
 });

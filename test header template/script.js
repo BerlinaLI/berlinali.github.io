5cb2ef490a905c661640a5ada80a4746ebaabf44
berlina <berlina.lili@gmail.com>
2017-03-13 19:04:08 +0800

    add nav work

--- test header template/script.js
+++ test header template/script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
    $('#nav-container').load('https://berlinali.github.io/header #nav');
    setTimeout(function(){ 
    	 $('a.navresume').addClass("active");
+   	 $('a.navwork').addClass("active");
    }, 0);
 });

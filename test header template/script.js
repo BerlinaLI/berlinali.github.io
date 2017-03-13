0f712bcf1a5f3d4085cb1d3441e4497c3c5cba09
berlina <berlina.lili@gmail.com>
2017-03-13 19:00:16 +0800

    Revert "test resume"
    This reverts commit eb2e42659bfdbc8e00de5bd59cf86c97f0a99e10.

--- test header template/script.js
+++ test header template/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
    $('#nav-container').load('https://berlinali.github.io/header #nav');
-   setTimeout(function(){ 
+   // setTimeout(function(){ 
    	 $('a.navwork').addClass("active");
-   	 $('a.navresume').addClass("active");
-   }, 0);
+   // }, 0);
 });

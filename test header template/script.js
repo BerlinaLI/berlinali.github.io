91c432f51e81ebfe6c55422747e65127f7a6e4fe
berlina <berlina.lili@gmail.com>
2017-03-14 17:23:59 +0800

    change header.html url

--- test header template/script.js
+++ test header template/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   $('#nav-container').load('https://berlinali.github.io/header #nav');
+   $('#nav-container').load('https://berlinali.github.io/test%20header%20template/header.html #nav');
    setTimeout(function(){ 
    	 $('a.navresume').addClass("active");
    	 $('a.navwork').addClass("active");

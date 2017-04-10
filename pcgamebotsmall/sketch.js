9c30278279fb8f20cf8f2b9e1a79ff58efea0c99
berlina <berlina.lili@gmail.com>
2017-04-11 04:24:17 +0800

    small version

--- pcgamebotsmall/sketch.js
+++ pcgamebotsmall/sketch.js
@@ -1,7 +1,13 @@
-var n = 3; // /n
+
+/*
+this project also can  be viewed on 
+https://berlinali.github.io/pcgamebot/
+*/
+
+
 var balls = [];
 var teeth;
-var total = 10;
+var total = 20;
 var mic;
 var micBall;
 var lives;
@@ -46,7 +52,7 @@ function setup(){
 	mic.start();
 	
 	fill(255);
-	textSize(50/n);
+	textSize(50);
 	textAlign(CENTER,CENTER);
 	textFont(myFont);
 	
@@ -57,7 +63,7 @@ function draw(){
 	stableCanvas();
 	fill(255);
 	stroke(0);
-	strokeWeight(5/n);
+	strokeWeight(5);
 
 //reset with defaultMode before starting from state 1 again
 	if(state == 0){
@@ -85,8 +91,8 @@ function stableCanvas(){
 }
 
 function shakeCanvas(){
-	canvasX = random(-1,9/n);
-	canvasY = random(-9/n,1);
+	canvasX = random(-1,9);
+	canvasY = random(-9,1);
   canvas.position(canvasX,canvasY);
   //background(255); 加了background就不动了。。。
 }
@@ -137,7 +143,7 @@ function drawPlaying(){
   playText();
   playTime();
 	//lose
-	if(lives == 0 || micBall.y > height-20/n-micBall.size/2){
+	if(lives == 0 || micBall.y > height-20-micBall.size/2){
 		state = 2;
 	}
 	//win
@@ -157,7 +163,7 @@ function playAgain(){
 	playText();
   playTimeAgain();
 	//lose
-	if(lives == 0  || micBall.y > height-20/n-micBall.size/2){
+	if(lives == 0  || micBall.y > height-20-micBall.size/2){
 		state = 2;
 	}
 	//win
@@ -189,14 +195,13 @@ function winGame(){
 function beginText(){
 	fill(255);
 	stroke(0);
-	strokeWeight(6/n);
-	//textSize(40/n);
-	//text("Sound Reactive Mini Game",0,0,width,height-200/n);
+	strokeWeight(6);
+	textSize(40);
+	text("Sound Reactive Mini Game",0,0,width,height-200);
 	strokeWeight(3);
-	textSize(40/n);
+	textSize(17);
 	if(frameCount % 60 < 45){
-		// text("Press any key to play!",0,200/n,width,height-200/n);
-			text("*** CLICK ON ME! *** \n \n Press any key to play!",width/2, height/2);
+		text("Press any key to play!",0,200,width,height-200);
 	}
 	textStyle(NORMAL);
 	fill('rgba(0,0,255, 0.75)');
@@ -208,31 +213,31 @@ function beginText(){
 function playText(){
 	stroke(0);
 	textAlign(RIGHT);
-	textSize(20/n);
+	textSize(20);
 	if(lives <= 2){
     fill("red");
   } else{
     fill("white");
   }
-	text("Lives : "+lives,0,0,width-20/n,80/n);
+	text("Lives : "+lives,0,0,width-20,80);
 	fill(255);
-	text("Score : "+score,0,0,width-20/n,150/n)
+	text("Score : "+score,0,0,width-20,150)
 }
 
 function playTime(){
 	timer2 = millis();
 	ttimer = timer2 - timer1;
 	timer = round(ttimer/1000);
-	textSize(20/n);
-	text(timer+" sec",0,0,width-20/n,220/n);
+	textSize(20);
+	text(timer+" sec",0,0,width-20,220);
 }
 
 function playTimeAgain(){
 	timer4 = millis();
 	ttimer = timer4 - timer3;
 	timer = round(ttimer/1000);
-	textSize(20/n);
-	text(timer+" sec",0,0,width-20/n,220/n);
+	textSize(20);
+	text(timer+" sec",0,0,width-20,220);
 }
 
 function loseText(){
@@ -240,27 +245,27 @@ function loseText(){
 	textAlign(CENTER,CENTER);
 	textStyle(ITALIC);
 	stroke(0);
-	strokeWeight(6/n);
-	textSize(30/n);
-	text("Game over ! Your score is "+score+".",0,0,width,height-250/n);
-	text("It has played"+" "+timer+" secs.",0,0,width,height-100/n);
-	textSize(20/n);
-	strokeWeight(3/n);
+	strokeWeight(6);
+	textSize(30);
+	text("Game over ! Your score is "+score+".",0,0,width,height-250);
+	text("It has played"+" "+timer+" secs.",0,0,width,height-100);
+	textSize(20);
+	strokeWeight(3);
 	if(frameCount % 60 < 45){
-		text( "Press any key to play again !",0,0,width,height+100/n);
+		text( "Press any key to play again !",0,0,width,height+100);
 	}
 }
 
 function winText(){
 	fill(255);
 	stroke(0);
-	strokeWeight(6/n);
+	strokeWeight(6);
 	textAlign(CENTER,CENTER);
-	textSize(30/n);
-	text("Congrats ! Your score is "+score+".",0,0,width,height-250/n);
-	text("It has played"+" "+timer+" secs.",0,0,width,height-100/n);
-	textSize(20/n);
-	strokeWeight(3/n);
+	textSize(30);
+	text("Congrats ! Your score is "+score+".",0,0,width,height-250);
+	text("It has played"+" "+timer+" secs.",0,0,width,height-100);
+	textSize(20);
+	strokeWeight(3);
 	if(frameCount % 60 < 45){
 		text("Press any key to play again !",0,0,width,height+100);
 	}
@@ -268,13 +273,13 @@ function winText(){
 
 function Ball(micBall){
 	this.micBall = micBall;
-	this.size = 40/n;
-	this.speed = 10/n;
-	this.goodBall = random(0,100/n) < 60/n;
+	this.size = 40;
+	this.speed = 10;
+	this.goodBall = random(0,100) < 60;
 	
 	this.init = function(){
-		this.x = random(-width,-20/n);
-		this.y = random(this.size,height-25/n-this.size*2);
+		this.x = random(-width,-20);
+		this.y = random(this.size,height-25-this.size*2);
 	}
 	this.init();
 	
@@ -313,13 +318,13 @@ function Ball(micBall){
 	
 	this.flatMouth = function(){
 		fill(0);
-		rect(this.micBall.x-7/n,this.micBall.y+10/n,14/n,5/n);
+		rect(this.micBall.x-7,this.micBall.y+10,14,5);
 	}
 	
 	this.hit = function(){
 		stroke(0);
 		fill(255);
- 		ellipse(this.micBall.x,this.micBall.y+11/n,10/n,14/n);
+ 		ellipse(this.micBall.x,this.micBall.y+11,10,14);
 		lives --;
 		this.micBall.color = color(255,0,0);
 		hitSound.setVolume(0.5);
@@ -328,7 +333,7 @@ function Ball(micBall){
 	
 	this.score = function(){
 		fill(0);
-    triangle(this.micBall.x-8/n,this.micBall.y+8/n,this.micBall.x,this.micBall.y+18/n,this.micBall.x+8/n,this.micBall.y+8/n);
+    triangle(this.micBall.x-8,this.micBall.y+8,this.micBall.x,this.micBall.y+18,this.micBall.x+8,this.micBall.y+8);
 		score ++;
 		this.micBall.color = color(0,0,255);
 		scoreSound.setVolume(0.5);
@@ -339,10 +344,10 @@ function Ball(micBall){
 function MicBall(){
 	this.x = width/2;
 	this.y = height*3/4;
-	this.size = 50/n;
-	this.ssize = 15/n;
+	this.size = 50;
+	this.ssize = 15;
 	this.color = color(255);
-	this.speed = 3/n;
+	this.speed = 3;
   this.hr = random(0.11,0.12);
 
 	// this.init = function(){ //经常变的值
@@ -353,14 +358,14 @@ function MicBall(){
 		// this.init();
 		fill(this.color);
 		stroke(0);
-		strokeWeight(5/n);
+		strokeWeight(5);
 
 		ellipse(this.x,this.y,this.size,this.size);
 		this.color = color(255);
 		fill(0);
 		noStroke();
-		ellipse(this.x-10/n,this.y-5/n,this.ssize,this.ssize);
-		ellipse(this.x+10/n,this.y-5/n,this.ssize,this.ssize);
+		ellipse(this.x-10,this.y-5,this.ssize,this.ssize);
+		ellipse(this.x+10,this.y-5,this.ssize,this.ssize);
 	//change
 		fill(this.color);
 	}
@@ -368,10 +373,10 @@ function MicBall(){
 	this.update = function(){
 		framePlaying = frameCount;
 		var frame1 = framePlaying-frameIntro ;
-		textSize(100/n);
+		textSize(100);
 		textAlign(CENTER,CENTER);
 		stroke(0,127+127*sin(frameCount* this.hr));
-    strokeWeight(10/n);
+    strokeWeight(10);
 		if(frame1 < 20){
 			text("3",0,0,width,height);
 		}
@@ -424,7 +429,7 @@ function Teeth(){
     for(var i = 0 ; i < width/20 ;i++){
       fill(255);
       stroke(125,127+127*sin(frameCount* this.hr));
-      strokeWeight(3/n);
+      strokeWeight(3);
       triangle(i*this.slength,height,i*this.slength + this.slength/2,height-20,(i+1)*this.slength,height);
     }
   }

ccd90c6cec13f4e491b57dc0e4f3eecb8283e589
berlina <berlina.lili@gmail.com>
2017-04-11 11:13:17 +0800

    add ibmcomputer

---
+++ ibmcomputer/js/index.js
@@ -0,0 +1,5 @@
+//when the image is clicked, the background change to gaming. 
+
+// document.getElementById("gameinterface").addEventListener("click", function(){
+//   alert("***GAME INSTRUCTION***\n 1.Use your voice to control the game. \n 2.You only have 5 lives. Once hit on a red ball, you lose one life. \n\n Let's get started!")
+// });

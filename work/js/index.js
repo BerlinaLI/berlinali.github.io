02a3717f3b83da8010ba489ece0def80fcd34830
berlina <berlina.lili@gmail.com>
2017-04-19 17:19:39 +0800

    test delete active anchor

--- work/js/index.js
+++ work/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $('#nav-container').load('https://berlinali.github.io/header/');
    $('#footer-container').load('https://berlinali.github.io/footer/');
-   setTimeout(function(){ 
-   	 $('a.navwork').addClass("active");
-   }, 0);
+   // setTimeout(function(){ 
+   // 	 $('a.navwork').addClass("active");
+   // }, 0);
 });

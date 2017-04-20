9cc22716bb7844510b65373dd68ec6361ea08cf8
berlina <berlina.lili@gmail.com>
2017-04-20 11:02:49 +0800

    add new nav

---
+++ taobao/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   $('#nav-container').load('https://berlinali.github.io/header/index.html');
+
+   setTimeout(function(){ 
+   	 $('a.navwork').addClass("active");
+   }, 0);
+   
+
+});

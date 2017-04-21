06e7a4475d363d7e5cc82e73f66defa9caf6a3cf
berlina <berlina.lili@gmail.com>
2017-04-21 14:08:35 +0800

    color

--- resume/js/index.js
+++ resume/js/index.js
@@ -1,6 +1,16 @@
 $(document).ready(function() { 
-   $('#footer-container').load('https://berlinali.github.io/footer/index.html');
-     setTimeout(function(){ 
+  
+  setTimeout(function(){ 
     TweenMax.staggerTo(".col-md-5", 0.25, {'opacity': 1,  x: 0}, .25);   
-  }, 0.5);
+  }, 0.25);
+
+  setTimeout(function(){ 
+    TweenMax.staggerTo(".col-md-4", 0.4, {'opacity': 1,  x: 0}, .25);   
+  }, 0.4);
+
+  setTimeout(function(){ 
+    TweenMax.staggerTo(".col-md-6", 0.6, {'opacity': 1,  x: 0}, .25);   
+  }, 0.6);
+
 });
+

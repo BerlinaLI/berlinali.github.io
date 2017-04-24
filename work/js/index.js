ecd083d9ff09c5d17caf13479025dd8f8fc8ae01
berlina <berlina.lili@gmail.com>
2017-04-24 12:58:23 +0800

    add delete icon for pop-up window

--- work/js/index.js
+++ work/js/index.js
@@ -15,42 +15,84 @@ $(document).ready(function() {
 
 });
 
+//pop-up windows when click video/game 
+// var $overlay = $('<div class="overlay"></div>');
+// var $popup =  $('<div></div>');
+// var $content = $('<iframe class="videoiframe"  src="" frameborder="0" allowfullscreen></iframe>');
+// var elevatorVideo="https://www.youtube.com/embed/WIFG6bIdo7Q";
+// var taobaoVideo ="https://www.youtube.com/embed/fiJgCSeNmD4";
+// var gameLink= "https://berlinali.github.io/ibmcomputer/";
+
+// $overlay.append($popup);
+// $popup.append($content);
+// $('body').append($overlay);
+
+// $(document).ready(function() {
+//   //open pop-up windows
+//   $('#pop-it-elevator, #elevator').click(function(){
+//     $(".videoiframe").attr("src",elevatorVideo);
+//     $overlay.show();  
+//   });
+
+//   $('#pop-it-bot, #taobao').click(function(){
+//     $(".videoiframe").attr("src",taobaoVideo);
+//     $overlay.show();  
+//   });
+  
+//   $('#pop-it-game, #mindplus').click(function(){
+//     $(".videoiframe").attr("src",gameLink);
+//     $(".videoiframe").css({"width":"560px","height":"815px","overflow":"visible"});
+//     $overlay.show();  
+//   });
+
+// //close pop-up windows
+//   $overlay.click(function() {
+//     $overlay.hide();
+//     $(".videoiframe").attr("src","");
+//     $(".videoiframe").css({"width":"560px","height":"315px","overflow":"visible"});
+//   });
+// });
+
 
 var $overlay = $('<div class="overlay"></div>');
 var $popup =  $('<div></div>');
 var $content = $('<iframe class="videoiframe"  src="" frameborder="0" allowfullscreen></iframe>');
+var $deleteIcon = $('<div class="deleteIcon"><i class="fa fa-close"></i></div>');
+
 var elevatorVideo="https://www.youtube.com/embed/WIFG6bIdo7Q";
 var taobaoVideo ="https://www.youtube.com/embed/fiJgCSeNmD4";
 var gameLink= "https://berlinali.github.io/ibmcomputer/";
 
+
 $overlay.append($popup);
 $popup.append($content);
+$popup.append($deleteIcon);
 $('body').append($overlay);
 
 $(document).ready(function() {
   //open pop-up windows
-    $('#pop-it-elevator, #elevator').click(function(){
-      $(".videoiframe").attr("src",elevatorVideo);
-      $overlay.show();  
-    });
+  $('#pop-it-elevator, #elevator').click(function(){
+    $(".videoiframe").attr("src",elevatorVideo);
+    $overlay.show();  
+  });
 
-    $('#pop-it-bot, #taobao').click(function(){
-      $(".videoiframe").attr("src",taobaoVideo);
-      $overlay.show();  
-    });
-    
-    $('#pop-it-game, #mindplus').click(function(){
-      $(".videoiframe").attr("src",gameLink);
-      $(".videoiframe").css({"width":"560px","height":"815px","overflow":"visible"});
-      $overlay.show();  
-    });
+  $('#pop-it-bot, #taobao').click(function(){
+    $(".videoiframe").attr("src",taobaoVideo);
+    $overlay.show();  
+  });
+  
+  $('#pop-it-game, #mindplus').click(function(){
+    $(".videoiframe").attr("src",gameLink);
+    $(".videoiframe").css({"width":"560px","height":"815px","overflow":"visible"});
+    $overlay.show();  
+  });
 
-  //close pop-up windows
-    $overlay.click(function() {
-      $overlay.hide();
-      $(".videoiframe").attr("src","");
-      $(".videoiframe").css({"width":"560px","height":"315px","overflow":"visible"});
-    });
+//close pop-up windows
+  $overlay.click(function() {
+    $overlay.hide();
+    $(".videoiframe").attr("src","");
+    $(".videoiframe").css({"width":"","height":"","overflow":""});
+  });
 });
 
 

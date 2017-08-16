fb22ef7c042d5b70e1163c1925436f83043075fe
berlina <berlina.lili@gmail.com>
2017-08-16 18:35:26 +0800

    add link button

--- work3/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/berlinali/pen/e292d7286a24aa0445290c15ca859fbe.
-
- 
--- work3/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-   $('#nav-container').load('https://berlinali.github.io/header/index.html');
-
-   setTimeout(function(){ 
-   	 $('a.navwork').addClass("active");
-   }, 0);
-   
-   $('#footer-container').load('https://berlinali.github.io/footer/index.html');
-});

dfe578a7ec9c2de2b8675243d0cd6c1bf064cd54
berlina <berlina.lili@gmail.com>
2017-09-27 18:12:31 +0800

    finish optimization except csv exportation

--- py/artwanted/loop_multpages.py
+++ py/artwanted/loop_multpages.py
@@ -17,34 +17,28 @@ import re
 number = 10000
 validNum = 0
 s1  = time.time()
-#for i in range(10000,81000):
 
-for i in range(86400, 86398, -1):	
-# try:
-
+for i in range(84400, 0, -1):	
   page = urllib2.urlopen('http://www.artwanted.com/artist.cfm?ArtID={}'.format(i)+'&Tab=Contact')
-  #except:
-  #continue
-  #else:
   data=[]
   content = page.read()
   soup = BeautifulSoup(content,'html.parser')
   name = soup.h1.getText().strip()
+  #link = soup.find(string = re.compile("www."))
+  linkArr = soup.find_all(string = re.compile("www."))
   link = soup.find_all(string = re.compile("www."))[0]
-  #wwwLen = len(soup.find_all(string = re.compile("www.")))
-  #hasNoPersonalWebsite = soup.find_all("a", string="http://www.artwanted.com")
-  #lens=len(hasNoPersonalWebsite)
+  lensLink = len(linkArr)
+  #print (link)
 
-  #if name != "Browse Artwork" and lens != 1 and wwwLen != 1:
-  if name and link:
+  if name and lensLink!=1:
    validNum = validNum + 1
    data.append((name, link))
-   print (name,link,validNum,"/",86401-i)
+   print (name,link,validNum,"/",84401-i)
 
-  # with open('index.csv', 'a', encoding='utf-8') as csv_file:
-   # writer = csv.writer(csv_file)
-   # for name, link in data:
-#     writer.writerow([name, link])
+   with open('index.csv', 'a', encoding='utf-8') as csv_file:
+    writer = csv.writer(csv_file)
+    for name, link in data:
+     writer.writerow([name, link])
 
   number = number - 1
 

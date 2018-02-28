a1c52952e3423e402520a53d8ae7f351bb4e224c
berlina <berlina.lili@gmail.com>
2018-02-28 16:27:37 +0800

    keyboard UI w/out result

--- calculatorUI/js/index.js
+++ calculatorUI/js/index.js
@@ -1,12 +1,58 @@
-var num = [1,2,3,"+","-","="];
+var keys = ["c","%","÷","x",7,8,9,"-",4,5,6,"+",1,2,3,"=",0,".","+/-"];  
+var dataKeys = [];
 
-var socialList = function() {
+var createKeyboardDom = function() {
   var output = '<ul>', 
-    myList =document.getElementById("keyboard-wrapper");
-  for(var i=0; i< num.length; i++){
-    output+= '<li><button>' + num[i] + '</button></li>';
+    keyboardList =document.getElementById("keyboard-wrapper");
+  
+  for(var i=0; i< keys.length; i++){
+  	//convert keyboard text to data for special case  
+    switch (keys[i]) {
+	    case "c":
+	        dataKeys[i] = "clear";
+	        break; 
+	    case "÷":
+	        dataKeys[i] = "/";
+	        break; 
+	    case "x":
+	        dataKeys[i] = "*";
+	        break; 
+	    default: 
+	        dataKeys[i] = keys[i];
+		}
+    output+= '<li><button class="button" data=' + dataKeys[i] + '>' + keys[i] + '</button></li>';			
   }
   output+= '</ul>';
 
-  myList.innerHTML = output;
+  keyboardList.innerHTML = output;
+
+  
+
 }();
+
+var createDomStyling = function(){
+	var buttons = document.getElementsByClassName("button");
+	var i;
+	for (i = 0; i < buttons.length; i++) {
+		var button = buttons[i];
+		var dataAttr = button.getAttribute("data");
+		if(dataAttr == "="){
+			button.style.backgroundColor = "orange";
+		}
+		//if it is a number,background color is grey;
+		var matches = dataAttr.match(/\d+/g);
+		if (matches != null) {
+			button.style.backgroundColor = "#999";
+		}
+	}
+}();
+
+
+
+
+
+// var x = document.getElementsByClassName("button");
+// var i;
+// for (i = 0; i < x.length; i++) {
+//     x[i].style.backgroundColor = "red";
+// }

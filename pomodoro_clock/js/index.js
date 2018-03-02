7500c2b3b335f85612eed6cff94b5f02ddfbea73
berlina <berlina.lili@gmail.com>
2018-03-02 18:51:36 +0800

    finish play & pause feature

--- pomodoro_clock/js/index.js
+++ pomodoro_clock/js/index.js
@@ -1,9 +1,16 @@
+//1. pause interval 
+//https://stackoverflow.com/questions/21277900/javascript-pausing-setinterval/21278007#21278007
+
+
+
+
 var workArr = [0,3];
-var breakArr = [0,2];
+var breakArr = [0,5];
 var timeArr = workArr;
 var min = timeArr[0];
 var sec = timeArr[1];
 var runWorkArr = true;
+var getTime;
 
 
 function displayTime(){
@@ -33,54 +40,41 @@ function countDown(){
       min = breakArr[0];
       sec = breakArr[1];
       runWorkArr= false;
+      $(".status").text("break :" + breakArr[1] +"s");
       $(".main").css("background-color", "yellow");
     }else{
       min = workArr[0];
       sec = workArr[1];
       runWorkArr= true;
-       $(".main").css("background-color", "red");
+      $(".status").text("work" + workArr[1] +"s");
+      $(".main").css("background-color", "red");
     }
   }
 }
 
+$(".status").text("work" + workArr[1] +"s");
+$(".main").css("background-color", "red");
+
+var isPaused = false;
+var t = window.setInterval(function() {
+  if(!isPaused) {
+    countDown();
+  }
+}, 1000);
+
+$('.play').on('click', function(e) {
+  console.log("play");
+  e.preventDefault();
+  isPaused = false;
+});
+
+$('.pause').on('click', function(e) {
+  console.log("pause");
+  e.preventDefault();
+  isPaused = true;
+});
 
 
 
 
 
-// function Timer(callback, delay) {
-//     var timerId, start, remaining = delay;
-
-//     this.pause = function() {
-//         window.clearTimeout(timerId);
-//         remaining -= new Date() - start;
-//     };
-
-//     this.resume = function() {
-//         start = new Date();
-//         window.clearTimeout(timerId);
-//         timerId = window.setTimeout(callback, remaining);
-//     };
-
-//     this.resume();
-// }
-
-// var timer = new Timer(function() {
-//     alert("Done!");
-// }, 1000);
-
-
-var getTime = setInterval(function(){countDown();}, 1000);
-
-
-// $(".play").click(
-  
-
-//     var getTime = setInterval(function(){countDown();}, 1000);
-// );
-
-
-
-// timer.pause();
-
-// timer.resume();

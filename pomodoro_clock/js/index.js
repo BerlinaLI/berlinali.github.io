856be2fc30ba8a05f10a0174bcbfd83e01c5a933
berlina <berlina.lili@gmail.com>
2018-03-02 17:52:19 +0800

    switch from break and work  time

--- pomodoro_clock/js/index.js
+++ pomodoro_clock/js/index.js
@@ -1,34 +1,86 @@
-var timeArr = [1,2];
+var workArr = [0,3];
+var breakArr = [0,2];
+var timeArr = workArr;
 var min = timeArr[0];
 var sec = timeArr[1];
-var time = 0;
+var runWorkArr = true;
+
+
+function displayTime(){
+  var timerDisplay = convertTime(min) + ":" + convertTime(sec);
+  $(".timer").text(timerDisplay);
+}
 
 function convertTime(num){
-  if(num < 10 && num > 0){
+  if(num < 10 && num >= 0){
     return num = "0" + num;
+  }else{
+    return num;
   }
 }
 
-function displayTime(){
-  //var timerDisplay = convertTime(min) + ":" + convertTime(sec);
-  var timerDisplay = min + ":" + sec;
-  $(".timer").text(timerDisplay);
-  //  document.getElementById("demo").innerHTML = t;
-}
-//every 1 sec, add 1
 function countDown(){
   sec--;
   if(sec== -1){
     sec = 59;
     min --;
   }
-  displayTime();
+  
+  displayTime();  
   
   if(sec == 0 && min == 0){
-    console.log("stop");
-    clearInterval(getTime);
+    if(runWorkArr){
+      min = breakArr[0];
+      sec = breakArr[1];
+      runWorkArr= false;
+      $(".main").css("background-color", "yellow");
+    }else{
+      min = workArr[0];
+      sec = workArr[1];
+      runWorkArr= true;
+       $(".main").css("background-color", "red");
+    }
   }
-  
 }
 
+
+
+
+
+
+// function Timer(callback, delay) {
+//     var timerId, start, remaining = delay;
+
+//     this.pause = function() {
+//         window.clearTimeout(timerId);
+//         remaining -= new Date() - start;
+//     };
+
+//     this.resume = function() {
+//         start = new Date();
+//         window.clearTimeout(timerId);
+//         timerId = window.setTimeout(callback, remaining);
+//     };
+
+//     this.resume();
+// }
+
+// var timer = new Timer(function() {
+//     alert("Done!");
+// }, 1000);
+
+
 var getTime = setInterval(function(){countDown();}, 1000);
+
+
+// $(".play").click(
+  
+
+//     var getTime = setInterval(function(){countDown();}, 1000);
+// );
+
+
+
+// timer.pause();
+
+// timer.resume();
